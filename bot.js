290a5c1d395f94b09fe53c0f0d34f6376e59f48f
healong <37354105+healong@users.noreply.github.com>
2018-03-28 00:53:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'link') {
+    	message.reply('https://discord.gg/NmvjhSr');
+  	}
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
